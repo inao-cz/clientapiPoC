23079db5001f411492ddb7710f8adf71831a9114
Jakub <onemoreplayscz@gmail.com>
2021-06-07 19:26:48 +0200

    Changed to NewHope algo

--- src/main/java/me/inao/ecdhexample/KeyExchange.java
+++ src/main/java/me/inao/ecdhexample/KeyExchange.java
@@ -1,74 +1,24 @@
 package me.inao.ecdhexample;
 
 import lombok.Getter;
-import org.bouncycastle.jce.ECNamedCurveTable;
-import org.bouncycastle.jce.spec.ECParameterSpec;
-import org.bouncycastle.jce.spec.ECPrivateKeySpec;
-import org.bouncycastle.jce.spec.ECPublicKeySpec;
-import javax.crypto.KeyAgreement;
-import java.math.BigInteger;
-import java.security.*;
-import java.security.spec.ECGenParameterSpec;
+import org.bouncycastle.pqc.crypto.ExchangePair;
+import org.bouncycastle.pqc.crypto.newhope.NHExchangePairGenerator;
+import org.bouncycastle.pqc.crypto.newhope.NHPublicKeyParameters;
+import org.bouncycastle.util.encoders.Base64;
+
+import java.security.SecureRandom;
 
 public class KeyExchange {
+    ExchangePair pair;
     @Getter
-    KeyPair pair;
-    public void initKeys(){
-        try{
-            KeyPairGenerator generator = KeyPairGenerator.getInstance("ECDH", "BC");
-            generator.initialize(new ECGenParameterSpec("secp256k1"), new SecureRandom());
-            pair = generator.generateKeyPair();
-        }catch(Exception e){
-            pair = null;
-            e.printStackTrace();
-        }
+    private byte[] sharedVal = null;
+    public void doKeyExchange(String encodedPublicKeyServer) {
+        NHPublicKeyParameters params = new NHPublicKeyParameters(Base64.decode(encodedPublicKeyServer));
+        pair = new NHExchangePairGenerator(new SecureRandom()).generateExchange(params);
+        sharedVal = pair.getSharedValue();
     }
 
-    public PublicKey getPubKeyFromBytes(byte[] raw){
-        ECParameterSpec paramsSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
-        ECPublicKeySpec pubKeySpec = new ECPublicKeySpec(paramsSpec.getCurve().decodePoint(raw), paramsSpec);
-        try{
-            KeyFactory factory = KeyFactory.getInstance("ECDH", "BC");
-            return factory.generatePublic(pubKeySpec);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public PrivateKey getPrivKeyFromBytes(byte[] raw){
-        ECParameterSpec paramsSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
-        ECPrivateKeySpec privKeySpec = new ECPrivateKeySpec(new BigInteger(raw), paramsSpec);
-        try{
-            KeyFactory factory = KeyFactory.getInstance("ECDH", "BC");
-            return factory.generatePrivate(privKeySpec);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public byte[] calculateKey(byte[] privKey, byte[] pubKey){
-        try{
-            KeyAgreement secretAgreement = KeyAgreement.getInstance("ECDH", "BC");
-            secretAgreement.init(getPrivKeyFromBytes(privKey));
-            secretAgreement.doPhase(getPubKeyFromBytes(pubKey), true);
-            return secretAgreement.generateSecret();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public byte[] calculateKey(byte[] pubKey){
-        try{
-            KeyAgreement secretAgreement = KeyAgreement.getInstance("ECDH", "BC");
-            secretAgreement.init(pair.getPrivate());
-            secretAgreement.doPhase(getPubKeyFromBytes(pubKey), true);
-            return secretAgreement.generateSecret();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
+    public String getEncodedPublicKey() {
+        return new String(Base64.encode(((NHPublicKeyParameters) pair.getPublicKey()).getPubData()));
     }
 }
--- src/main/java/me/inao/ecdhexample/Main.java
+++ src/main/java/me/inao/ecdhexample/Main.java
@@ -1,9 +1,9 @@
 package me.inao.ecdhexample;
 
-import org.bouncycastle.jce.interfaces.ECPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+
 import java.security.Security;
-import java.util.Base64;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,17 +12,11 @@ public class Main {
     public void starter(){
         Security.addProvider(new BouncyCastleProvider());
         Connection conn = new Connection();
+        KeyExchange exchange = new KeyExchange();
         conn.init();
-        Session session = new Session();
-        session.getKeyexchange().initKeys();
         String pubKeyServer = conn.read();
-        conn.write(new String(Base64.getEncoder().encode(((ECPublicKey)session.getKeyexchange().getPair().getPublic()).getQ().getEncoded(true))));
-        session.setSecret(session.getKeyexchange().calculateKey(Base64.getDecoder().decode(pubKeyServer)));
-        System.out.println(Base64.getEncoder().encodeToString(session.getSecret()));
-        try{
-            conn.getConn().close();
-        }catch(Exception e1){
-            e1.printStackTrace();
-        }
+        exchange.doKeyExchange(pubKeyServer);
+        conn.write(exchange.getEncodedPublicKey());
+        System.out.println(new String(Base64.encode(exchange.getSharedVal())));
     }
 }
